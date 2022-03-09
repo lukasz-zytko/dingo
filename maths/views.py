6d69f16419f8c14587caa784d4a3e4f380d9c983
Lukasz Zytko <zytko.lukasz@gmail.com>
2022-03-09 17:17:13 +0100

    Add context and template

--- maths/views.py
+++ maths/views.py
@@ -1,23 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Context, loader
 
 def maths(request):
     return HttpResponse("Tu będzie matma")
 
 def add(request, a, b):
     a, b = int(a), int(b)
-    return HttpResponse(a + b)
+    wynik = a + b
+    c = {"a": a, "b": b, "operacja": "+", "wynik": wynik}
+    return render(
+        request=request,
+        template_name="maths/main.html",
+        context=c
+    )
 
 def sub(request, a, b):
     a, b = int(a), int(b)
-    return HttpResponse(a - b)
+    wynik = a - b
+    c = {"a": a, "b": b, "operacja": "-", "wynik": wynik}
+    return render(
+        request=request,
+        template_name="maths/main.html",
+        context=c
+    )
 
 def mul(request, a, b):
     a, b = int(a), int(b)
-    return HttpResponse(a * b)
+    wynik = a * b
+    c = {"a": a, "b": b, "operacja": "*", "wynik": wynik}
+    return render(
+        request=request,
+        template_name="maths/main.html",
+        context=c
+    )
 
 def div(request, a, b):
-    a, b = int(a), int(b)
+    a, b = int(a), int(b)  
     if b == 0:
-        return HttpResponse("Nie dzielimy przez 0")
-    return HttpResponse(a / b)
+        wynik = "Nie dzielimy przez 0!!!"
+    else:
+        wynik = a / b
+    c = {"a": a, "b": b, "operacja": "/", "wynik": wynik}
+    return render(
+        request=request,
+        template_name="maths/main.html",
+        context=c
+    )
