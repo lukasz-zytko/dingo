4b2ac1592245cfe403f8fb2a4c02fec22e75516a
Lukasz Zytko <zytko.lukasz@gmail.com>
2022-03-13 15:47:00 +0100

    Add blog posts and authors

---
+++ posts/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=65,
+        unique=True
+    )
+    content = models.TextField()
+    create = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(
+        auto_now=True,
+        blank=True,
+        null=True
+    )
+    author = models.ForeignKey(
+        "posts.Author",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True        
+    )
+    def __str__(self):
+        return f"Title: {self.title} | Content: {self.content} | Author-id: {self.author_id}"
+
+class Author(models.Model):
+    nick = models.CharField(max_length=25, unique=True)
+    bio = models.TextField(
+        blank=True,
+        null=True
+    )
+    email = models.EmailField()
+    def __str__(self):
+        return f"Nick: {self.nick} | email: {self.email} | Bio: {self.bio} "
