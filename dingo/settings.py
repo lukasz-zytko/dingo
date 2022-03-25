b0d0ab986947d23fcaf4f231c52a4a2dd3c0e95b
Lukasz Zytko <zytko.lukasz@gmail.com>
2022-03-25 23:37:34 +0100

    Add changes

--- dingo/settings.py
+++ dingo/settings.py
@@ -131,3 +131,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGOUT_REDIRECT_URL = '/maths/histories/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
